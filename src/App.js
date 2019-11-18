254e5c2f234344251eed4c94b04f947f8afa1cad
Mateusz Cholcha <mateusz.cholcha@gmail.com>
2019-11-18 02:29:48 +0100

    connected

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import Episodes from "./components/Episodes"
+import Loader from "./components/Loader"
+import {getEpisodes} from "./services/Api"
+import Pagination from "./components/Pagination"
 
-/**
- * The App  
- */
 
-function App(props) {
+function App() {
+  const [page, setPage] = useState(1)
+  const [allPages, setAllPages] = useState(1)
+  const [loading, setLoading] = useState(false)
+  const [episodes, setEpisodes] = useState([])
+
+  useEffect(() => {
+    setLoading(true)
+    getEpisodes(page).then(({episodes, pages}) => {
+      setEpisodes(episodes);
+      setAllPages(pages);
+      setLoading(false)
+    })
+  }, [page])
+
 
   return (
-    <main>
-      Hello Qunabu Escola team
+    <main className="side-container">
+      <Loader loading={loading}>
+        <Episodes episodes={episodes} />
+
+        <Pagination
+          page={page}
+          pages={allPages}
+          onChangePage={newPage => setPage(newPage)}
+        />
+      </Loader>
     </main>
   );
 }
